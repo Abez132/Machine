a6230f9aa436af9eb195cae92fd773a5d4ff885e
Abenezer daniel <abenezerdaniel06@gmail.com>
2025-07-24 20:41:28 +0300

    feat: add gh-pages support and update footer layout
    - Added gh-pages as a dependency for deployment.
    - Updated Footer component to include contact information and logo.
    - Added new assets including foot.jpg.
    - Modified Services component to include a thank you message.

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,7 +1,58 @@
+// import foot from "../assets/foot.jpg"
+import { MdEmail } from "react-icons/md";
+import { FaPhone } from "react-icons/fa6";
+
 const Footer=()=>{
     return(
-        <footer className="bottom-0 w-full bg-gradient-to-r from-indigo-400 to-indigo-800  p-4 text-center text-teal-700">
-            © 2023 Your Company. All rights reserved.
+        
+        <footer className=" flex bottom-0 w-full justify-between bg-gradient-to-r from-sky-900 to-sky-950  p-4 text-center text-amber-50" 
+        // style={{
+        //     backgroundImage:`url(${foot})`,
+        //     backgroundSize: "cover",
+        //     backgroundRepeat: "no-repeat",
+        //     backgroundPosition: "center",
+        //     imageRendering: "auto"
+        // }}
+        >
+            <div className="flex justify-center items-center" >
+                <img className="h-24" src="src/assets/HABYAT LOGO3.PNG" alt="logo" />
+                <p>HABYAT trading PLC</p>
+            </div>
+            <div className="flex flex-col  ">     
+            
+                <div
+                    className="flex items-center rounded "
+                    
+                >
+                    <FaPhone className="m-2 text-l" />
+                    <p className="m-3">+251 912 397 422</p>
+                </div>
+    
+                
+                <div
+                    className="flex items-center rounded "
+                    
+                >
+                    <FaPhone className="m-2 text-l" />
+                    <p className="m-3">+251 940 190 896</p>
+                </div>
+                
+                <div
+                    className="flex items-center rounded "
+                
+                >
+                    <FaPhone className="m-2 text-l" />
+                    <p className="m-3">+251 911 649 706</p>
+                </div>
+                <div className="flex items-center rounded " >
+                    <MdEmail className="m-2 text-l" />
+                    <p className="m-3">habyattradingplc@gmail.com</p>
+                </div>
+            </div>
+            <div>
+
+            </div>
+           
         </footer>
     );
 };
