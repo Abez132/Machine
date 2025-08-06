8b9e63933cf3eb340b18f8c54123ace10243c5cf
Abenezer daniel <abenezerdaniel06@gmail.com>
2025-08-06 17:50:55 +0300

    fixed the image not loading in vercel

--- src/layout/data/cards.js
+++ src/layout/data/cards.js
@@ -2,7 +2,7 @@
 
 export const cards = [
     {
-        image: "src/assets/IMG_7658.webp",
+        image: "src/assets/img.webp",
         title: "Great team work",
         desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
         detail:"lorem ipsum dolor sit amet consectetur adipisicing elit. Earum ea maxime porro doloribus praesentium ipsa vero possimus hic modi voluptatem esse, fugiat itaque saepe quia, obcaecati iusto quam, qui facilis."
