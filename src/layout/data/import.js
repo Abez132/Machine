1da59b2afa5c40112ba9faefbc7b57ca23ddd625
Abenezer daniel <abenezerdaniel06@gmail.com>
2025-08-07 12:51:58 +0300

    fixed missmaching data

--- src/layout/data/import.js
+++ src/layout/data/import.js
@@ -20,15 +20,16 @@ export const imports = [
     },
      {
         image: "https://images.pexels.com/photos/34693/escalator-metro-stairs-subway.jpg",
-        title: "Bulldozers & Wheel Loaders",
-        desc: "Powerful machines for pushing, digging, and loading materials on work sites.",
-        detail:"Our heavy-duty bulldozers and wheel loaders are essential for site preparation, earthmoving, and material handling. These machines combine power and precision, enabling smooth operation even in demanding environments. They are a key asset for roadwork, mining, and construction projects."
+        title: "Elevators & Escalators",
+        desc: "Modern vertical transport systems for buildings of all types and sizes.",
+        detail:"HABYAT Trading PLC imports modern elevator and escalator systems that prioritize safety, efficiency, and user comfort. Designed for residential, commercial, and industrial buildings, our solutions enhance vertical mobility while adhering to international safety and accessibility standards."
+        
     },
      {
         image: "https://i.pinimg.com/1200x/96/b3/09/96b309f44a8ac9bdb41c1e05fbc43eef.jpg",
-        title: " Elevators & Escalators",
-        desc: "Modern vertical transport systems for buildings of all types and sizes.",
-        detail:"HABYAT Trading PLC imports modern elevator and escalator systems that prioritize safety, efficiency, and user comfort. Designed for residential, commercial, and industrial buildings, our solutions enhance vertical mobility while adhering to international safety and accessibility standards."
+        title: "Bulldozers & Wheel Loaders",
+        desc: "Powerful machines for pushing, digging, and loading materials on work sites.",
+        detail:"Our heavy-duty bulldozers and wheel loaders are essential for site preparation, earthmoving, and material handling. These machines combine power and precision, enabling smooth operation even in demanding environments. They are a key asset for roadwork, mining, and construction projects."
     },
      
      {
