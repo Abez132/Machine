fa86564d475cc58d252dc5c77a8729f67243184d
Abenezer daniel <abenezerdaniel06@gmail.com>
2025-08-15 18:23:42 +0300

    some edites in animation

--- src/layout/Home.jsx
+++ src/layout/Home.jsx
@@ -5,6 +5,7 @@ import { HiUserGroup } from "react-icons/hi";
 import { GiStairsGoal } from "react-icons/gi";
 import { FaRegEye } from "react-icons/fa";
 import { motion as _motions } from "framer-motion";
+
 const Home=()=>{
 
     // const cards = [
@@ -53,7 +54,7 @@ const Home=()=>{
             <Hero/>
             <div className="flex flex-wrap justify-center gap-16 px-6 py-16 text-center ">
                 {acards.map((card, index) => (
-                  <_motions.div whileInView={{ opacity: 1 , scale:1}} initial={{ opacity: 0, scale:0.9 }} transition={{duration:0.5 }} key={index} className="max-w-sm flex  hover:shadow-stone-500 transition delay-1  shadow-lg shadow-stone-700 rounded p-2 flex-col justify-center items-center space-y-4">
+                  <_motions.div whileInView={{ opacity: 1 , scale:1}} initial={{ opacity: 0, scale:0.9 }} transition={{duration:0.3,delay:0.1 * index, ease: "linear", type: "tween"}} key={index} className="max-w-sm flex  hover:shadow-stone-500 transition delay-1  shadow-lg shadow-stone-700 rounded p-2 flex-col justify-center items-center space-y-4">
                         <div className="text-6xl  ">{card.image}</div>
                             <h3 className="text-2xl  text-gray-400 font-bold">{card.title}</h3>
                             <p className=" text-sm font-medium leading-relaxed">{card.desc}</p>
