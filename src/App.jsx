a5f8a30b63838eb3490b06b1e345e9b0156850d7
Abenezer daniel <abenezerdaniel06@gmail.com>
2025-08-19 14:35:26 +0300

    changed the bg color

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
     
 
   return (
-    <div id="top" className="relative p-0 m-0 text-white bg-cyan-950">
+    <div id="top" className="relative p-0 m-0 text-white bg-gradient-to-b from-amber-700 to-amber-500">
       {/* <Analytics/> */}
       <Router>
         
--- src/layout/Detaile.jsx
+++ src/layout/Detaile.jsx
@@ -9,7 +9,7 @@ const Detail=({image,detail,name})=>{
             </div>
 
             <div className="md:w-1/2 w-full p-2 ">
-                <h1 className="text-3xl text-stone-400 font-bold">{name}</h1>
+                <h1 className="text-3xl text-amber-300 font-bold">{name}</h1>
                 <p className="text-stone-100">{detail}</p>
                 <Link to="/contact"><button className="bg-sky-700 hover:bg-sky-400 hover:scale-105 active:scale-95 transition text-white px-4 py-2 m-2 rounded">Contact Us</button></Link>
             </div>
